73b993ac77d9428d722570db0404a430886471a5
Kluxx <111638795+Kluxx@users.noreply.github.com>
2023-07-04 02:54:42 +0300

    Delete bg.js

--- js/bg.js
+++
@@ -1,8 +0,0 @@
-// JavaScript code to set the background image
-
-const imageFile = 'images/image.jpg'; // Update this with your own image file path
-
-window.addEventListener('DOMContentLoaded', () => {
-  const container = document.querySelector('.container');
-  container.style.backgroundImage = `url('${imageFile}')`;
-});
